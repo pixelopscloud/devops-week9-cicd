43554e3d007fbfe4fb3de54e0adf96bf02dd9df2
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2026-09-08 03:17:14 +0500

    Update app to version 2.0

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3500;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from DevOps Week 9 CI/CD Pipeline! Version 1.0\n');
+  res.end('Hello from DevOps Week 9 CI/CD Pipeline! Version 2.0 - UPDATED!\n');
 });
 
 server.listen(PORT, () => {
